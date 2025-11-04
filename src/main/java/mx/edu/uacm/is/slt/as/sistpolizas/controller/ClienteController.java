79e5f2af8afd2c2e1671cb3a169a7c02ef6e96db
ElMonje1 <juan-135v@hotmail.com>
2025-11-04 00:52:32 -0600

    Se eliminan comentarios

--- src/main/java/mx/edu/uacm/is/slt/as/sistpolizas/controller/ClienteController.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistpolizas/controller/ClienteController.java
@@ -18,8 +18,9 @@ public class ClienteController {
     //Servicio REST GET
     @GetMapping("/cliente/{curp}")
     public ResponseEntity<String> getCliente(@PathVariable String curp){
-        //Cliente cliente = ClienteRepository.findByCurp(curp);
         //Aqui va la implementacion
+        //Cliente cliente = ClienteRepository.findByCurp(curp);
+
         //Cliente cliente = new Cliente();
         return ResponseEntity.ok("Cliente devuelto");
     }
@@ -49,37 +50,8 @@ public class ClienteController {
             @PathVariable String nombre,
             @PathVariable String p_apellido,
             @PathVariable (required = false)String s_apellido) {
+        //Aquí va la implementación
         //Mensaje de cliente actualizado
         return ResponseEntity.ok("Cliente actualizado");
-        /*Posibles validaciones
-        if (cliente == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no encontrado");
-        }
-        if(direccion == null || fechaNacimiento == null || nombres == null || primerApellido == null){
-            return ResponseEntity.badRequest().body("Existen campos nulos donde no deberia de haberlos");
-        }
-        if (segundoApellido != null && !segundoApellido.equals("")) {
-            cliente.setSegundoApellido(segundoApellido);
-        }
-
-        try {
-            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-            Date fecha = formato.parse(fechaNacimiento);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Formato de fecha inválido. Usa yyyy-MM-dd");
-        }*/
-
-        /*Posible implementacion
-        Cliente cliente = clienteRepository.findByCurp(curp);
-
-        cliente.setDireccion(direccion);
-        cliente.setFecha_nacimiento(fechaNacimiento);
-        cliente.setNombre(nombres);
-        cliente.setP_apellido(primerApellido);
-
-        clienteServicio.actualizarCliente(cliente);
-        */
-
-        //log.info("Cliente actualizado");
     }
 }
